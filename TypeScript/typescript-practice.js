4de3f90958c81b8458c88ac4a21958e4b13b4aa6
Dharmveer Singh <singhdharmveer311@gmail.com>
2026-04-02 07:31:48 +0200

    feat: Add TypeScript practice file with variable declarations and console logs

---
+++ TypeScript/typescript-practice.js
@@ -0,0 +1,7 @@
+"use strict";
+const nameValue = "Dharmveer Singh";
+// nameValue = "Raj".   this cannot be changed error
+let variableValue = "This can be changed";
+variableValue = "Changed";
+console.log(variableValue);
+// to run first convert typescript to javasciprt tsc filenem.ts and then run -> node new-file.js
---
+++ TypeScript/typescript-practice.ts
@@ -0,0 +1,22 @@
+const nameValue: string = "Dharmveer Singh"
+// nameValue = "Raj".   this cannot be changed error
+
+
+let variableValue: string = "This can be changed"
+variableValue = "Changed"
+console.log(variableValue)
+
+
+// to run first convert typescript to javasciprt tsc filenem.ts and then run -> node filenem.js
+
+const b: number // this you have to initialize and type declaration is needed
+b = 21
+
+let a
+a = 20
+
+
+// VARIABLES 
+let course: string = 'this is string '
+console.log(course)
+
