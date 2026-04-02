530d1b078abea90ae58cd40b129a81391173e66c
Dharmveer Singh <singhdharmveer311@gmail.com>
2026-04-02 10:33:01 +0200

    feat: Enhance TypeScript practice file with additional variable declarations, functions, and user creation examples

--- TypeScript/typescript-practice.js
+++ TypeScript/typescript-practice.js
@@ -1,7 +1,34 @@
-"use strict";
 const nameValue = "Dharmveer Singh";
 // nameValue = "Raj".   this cannot be changed error
 let variableValue = "This can be changed";
 variableValue = "Changed";
 console.log(variableValue);
-// to run first convert typescript to javasciprt tsc filenem.ts and then run -> node new-file.js
+// to run first convert typescript to javasciprt tsc filenem.ts and then run -> node filenem.js
+const b; // this you have to initialize and type declaration is needed
+b = 21;
+let a;
+a = 20;
+// VARIABLES 
+let course = 'this is string ';
+console.log(course);
+let nameVal = "Rah singh";
+let description = nameVal + 'is best ';
+console.log(description);
+// array 
+let users = ["a", "b", "c"];
+let users2 = ['a', 'b', '3']; // or (string | number )[]
+let marks = ['a', '1', '3'];
+console.log(users[1], users2[0], marks);
+function sayHello() {
+    console.log("askdlfn");
+}
+sayHello();
+const sayhello2 = () => {
+    console.log("this is also way of declaration of functions");
+};
+sayhello2();
+const multiple = (a, b) => {
+    return a * b;
+};
+console.log(multiple(3, 4));
+export {};
--- TypeScript/typescript-practice.ts
+++ TypeScript/typescript-practice.ts
@@ -1,4 +1,4 @@
-const nameValue: string = "Dharmveer Singh"
+const nameValue: string = "Dharmveer Singh";
 // nameValue = "Raj".   this cannot be changed error
 
 
@@ -20,3 +20,86 @@ a = 20
 let course: string = 'this is string '
 console.log(course)
 
+let nameVal: string = "Rah singh"
+let description: string = nameVal + 'is best '
+console.log(description)
+
+
+// array 
+let users: string[] = ["a", "b", "c"]
+let users2: Array<string | number> = ['a', 'b', '3']    // or (string | number )[]
+let marks: any[] = ['a', '1', '3']
+console.log(users[1], users2[0], marks)
+
+
+
+function sayHello() : void{
+    console.log("askdlfn")
+}
+
+
+sayHello()
+
+const sayhello2 = () : void => {
+    console.log("this is also way of declaration of functions")
+}
+
+sayhello2();
+
+
+const multiple = (a: number, b: number): number=>{
+    return a*b; 
+}
+console.log(multiple(3, 4));
+
+const mul2 = (a: number, b:number): number => a*b;
+
+
+function createUser(name: string, age: string) : {name: string, age: string} {
+    return { 
+        name, 
+        age
+    };
+}
+
+const createUser2 = (name: string, age: string): {name: string, age: string}=>{
+    return {name, age};
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {};
